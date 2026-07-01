0ab7d8050b96fef13ad9b422be04b3482f3359c6
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:31:19 +0530

    Add LDAP Injection detection test

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected LDAP Injection vulnerability
+const username = req.query.username;
+const filter = "(&(uid=" + username + "))";
+ldapClient.search('o=Example', { filter, scope: 'sub' }, (err, res) => {});
