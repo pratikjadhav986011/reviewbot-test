67aef01853712e61d36a17b39f3c1dd609a67529
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 22:55:08 +0530

    Add Path Traversal detection test

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Path Traversal vulnerability
+const filename = req.query.file;
+const fileContent = fs.readFileSync(path.join(__dirname, 'public', filename), 'utf8');
+res.send(fileContent);
