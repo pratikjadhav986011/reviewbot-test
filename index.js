7d75c099ac2303c406d8f6952635543ffa35919c
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:36:02 +0530

    Add Authentication Bypass detection test

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Authentication Bypass vulnerability
+if (req.headers['x-admin-bypass'] === 'true' || req.query.admin === '1') {
+  req.session.authenticated = true;
+  req.session.role = 'admin';
+}
