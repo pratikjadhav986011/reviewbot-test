f27398df8fe74d058464dc0f6512ffbb4914fe55
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:25:14 +0530

    Add Dangerous Process.Start() detection test

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Dangerous Process.Start() vulnerability (Node equivalent: spawn/exec unchecked)
+const userCmd = req.query.cmd;
+const { spawn } = require('child_process');
+spawn(userCmd, [], { shell: true });
