f2b9bfa18cdec50577ac24063920534e4e1a5632
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:07:43 +0530

    Add XPath Injection detection test

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected XPath Injection vulnerability
+const search = req.query.search;
+const xpath = require('xpath');
+const doc = new Dom().parseFromString(xml);
+const nodes = xpath.select("//user[username/text()='" + search + "']", doc);
