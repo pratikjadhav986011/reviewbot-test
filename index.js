5ed122cf548194022a7fec9f257d81bcc239e1ad
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:17:05 +0530

    Add SQL Injection detection test

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected SQL Injection vulnerability
+const userId = req.query.id;
+const query = "SELECT * FROM users WHERE id = '" + userId + "'";
+db.query(query, (err, results) => {
+  if (err) return res.status(500).json({ error: err.message });
+  res.json(results);
+});
