0c959d1985d56d75694426b7947daed668e1c8a6
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 22:59:29 +0530

    Add Hardcoded Secret detection test

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Hardcoded Secret vulnerability
+const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";
+const conn = new AWS.Connection({ secret: AWS_SECRET_KEY });
