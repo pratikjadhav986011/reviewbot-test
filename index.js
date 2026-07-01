5ce51e327517aee94e04ace444a553c6ce62683a
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:36:40 +0530

    Add Authorization Bypass detection test

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Authorization Bypass vulnerability
+const docId = req.query.id;
+// Missing ownership or role check, direct ID object reference (IDOR)
+const document = db.getDocument(docId);
+res.json(document);
