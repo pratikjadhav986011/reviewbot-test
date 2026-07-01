2c09ca3add25556884b1e15f5148c217db4f467a
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:32:53 +0530

    Add Template Injection detection test

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Template Injection vulnerability
+const ejs = require('ejs');
+const template = 'Hello <%= name %>';
+const rendered = ejs.render(template, { name: req.query.name });
