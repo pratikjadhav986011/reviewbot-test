55d4e610e4a5776ae9b4f7db7f9c369186540525
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-01 23:26:03 +0530

    Add Dangerous eval()/dynamic code execution detection test

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 console.log(add(2, 3));
+
+// Injected Dangerous eval() vulnerability
+const expression = req.query.expr;
+const result = eval(expression);
+res.json({ result });
