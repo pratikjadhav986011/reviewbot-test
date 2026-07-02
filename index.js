58bdb99ce89bea47dc99da95de3fa6c6d56a0530
Pratik Jadhav <pratikjadhav986011@gmail.com>
2026-07-02 09:00:35 +0530

    Inject SQL Injection for metadata test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function add(a, b) {
-  return a + b;
-}
-
-console.log(add(2, 3));
+// Injected SQL Injection vulnerability
+const userId = req.query.id;
+const query = "SELECT * FROM users WHERE id = '" + userId + "'";
+db.query(query, (err, results) => {
+  if (err) return res.status(500).json({ error: err.message });
+  res.json(results);
+});
